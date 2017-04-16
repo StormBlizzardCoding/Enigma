976c1651db14c2f7ae4a645bde3e879bc4d5a58a
poikNplop <hrmorley3.4@gmail.com>
2017-04-16 08:22:58 +0100

    Don't need to define all five rotors but need turnpoints.

--- Enigma.py
+++ Enigma.py
@@ -1,12 +1,12 @@
 from defaults import *
 
 class Enigma:
-    def __init__(self, rotors=(1, 2, 3), rotorloc=[0, 0, 0], rotor1=rotor1,
-                 rotor2=rotor2, rotor3=rotor3, rotor4=rotor4, rotor5=rotor5,
+    def __init__(self, rotorloc=[0, 0, 0], rotorturn=turnpoints[1:4],
+                 rotor1=rotor1, rotor2=rotor2, rotor3=rotor3,
                  endscrambler=reflect1, charset=rmaps[0], forcecaps=True):
         """ Create a new Enigma object.
 
-Note: Leave for Enigma defaults plus reflect 1.
+Defaults are Enigma rotors 1, 2 and 3 and reflector 1.
 
 Create a new Enigma object using different rotors. """
         
@@ -15,8 +15,6 @@ Create a new Enigma object using different rotors. """
         self._r1 = rotor1
         self._r2 = rotor2
         self._r3 = rotor3
-        self._r4 = rotor4
-        self._r5 = rotor5
         self._scre = endscrambler
 
     def _setupcharswap(self, charset, forcecaps):
